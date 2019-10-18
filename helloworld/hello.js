54023849832cfe4dddd2bfbd61be01ee496f22d5
Dave Winer <dave.winer@gmail.com>
2019-10-18 16:50:50 -0400

    .

--- helloworld/hello.js
+++ helloworld/hello.js
@@ -1,6 +1,6 @@
 const myProductName = "helloworld", myVersion = "0.4.1";    
 function everyMinute () { 
-	console.log ("\neveryMinute: " + new Date ().toLocaleTimeString ());
+	console.log ("\n" + myProductName + ": " + new Date ().toLocaleTimeString ());
 	}
 console.log ("\n" + myProductName + " v" + myVersion + "\n");
 utils.runEveryMinute (everyMinute);
